64e8c4f5aa32cd83f663af03fb92f1958266dbe8
manyagarg12344 <156235028+manyagarg12344@users.noreply.github.com>
2026-05-08 23:16:47 +0530

    first commit

---
+++ DAY1/VariableSizeSubarray.java
@@ -0,0 +1,18 @@
+public class VariableSizeSubarray {
+    
+    public static void main(String[] args) {
+        int[] arr = {2,1,5,1,3};
+        int k = 7;
+        int left = 0; int sum = 0; int maxLength = 0;
+
+        for(int right = 0; right < arr.length; right++){
+            sum+=arr[right];
+            while(sum>k){
+                sum-=arr[left];
+                left++;
+            }
+            maxLength = Math.max(maxLength, right -left+1 );
+        }
+        System.out.println("Maximum length of subarray with sum at least " + k + " is: " + maxLength);
+    }
+}
